cbd182dc2f50d293a2a4c7d38008ef03966e429b
GoDavai01 <info@godavaii.com>
2025-09-08 01:32:41 +0530

    fix it

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          // 2 years; add includeSubDomains only when all subdomains are HTTPS-ready
+          { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains" },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
